b39d45107fc1da5360be9f68e75b393867dca06d
Ziad Hesham <zizohesham20@gmail.com>
2024-04-18 02:58:07 +0200

    done

--- app/layout.js
+++ app/layout.js
@@ -3,16 +3,24 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "./providers";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GPTGenius",
-  description: 'GPTGenius: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!',
+  description: 'GPTGenius: Your AI language companion. Powered by EdenAI, it enhances your conversations, content creation, and magnage your Tours , and more!',
 };
 
 export default function RootLayout({ children }) {
   return (
+    
     <ClerkProvider>
+      
+      <meta
+      key={["Gpt Genuis","Chatgpt","openAi","tours","Tours managment"]}
+      />
+      
+      
       <html lang="en" data-theme='night' >
       <body className={inter.className}><Providers>
       {children}
