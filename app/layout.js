ba3f6974eb6a160e3d145826c3774c9b6527cd0a
Ziad Hesham <zizohesham20@gmail.com>
2024-04-18 19:13:11 +0200

    verfiy

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,22 @@ import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "./providers";
 
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GPTGenius",
   description: 'GPTGenius: Your AI language companion. Powered by EdenAI, it enhances your conversations, content creation, and magnage your Tours , and more!',
+  verification:{
+    google: 'google'
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     
     <ClerkProvider>
-      
+      <meta name="google-site-verification=oi_TwzJ9mVcOBzGAaXCS-Y6oXhJynx6f_KDKPv_ddGs" content="google" />
       <meta
       key={["Gpt Genuis","Chatgpt","openAi","tours","Tours managment"]}
       />
